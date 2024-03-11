293142dfd7d43b3bd2d217a98469d1031bd90c76
vishnududi-511 <dudivishnu4@gmail.com>
2024-03-11 09:24:06 +0530

    completed till section-8

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,5 +2,36 @@ let navitems = document.querySelector('.navitems');
 let menubox = document.querySelector('.menubox');
 
 menubox.addEventListener("click", function () {
-navitems.classList.toggle("show")
+    navitems.classList.toggle("show")
 })
+
+$('.sliderbox').slick({
+    dots: false,
+    infinite: false,
+    prevArrow: '.prev',
+    nextArrow: '.next',
+    speed: 300,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    infinite: true,
+    responsive: [
+        {
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 1,
+            }
+        },
+        {
+            breakpoint: 700,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
+        },
+
+        // You can unslick at a given breakpoint now by adding:
+        // settings: "unslick"
+        // instead of a settings object
+    ]
+});
