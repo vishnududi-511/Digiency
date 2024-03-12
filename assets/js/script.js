bc61cbd453b5c7acde6d48409a14ebd3aded33b6
vishnududi-511 <dudivishnu4@gmail.com>
2024-03-12 15:45:15 +0530

    scroll to top button added and footer completed

--- assets/js/script.js
+++ assets/js/script.js
@@ -16,17 +16,27 @@ $('.sliderbox').slick({
     infinite: true,
     responsive: [
         {
-            breakpoint: 1024,
+            breakpoint: 1400,
+            settings: {
+                autoplay: true,
+                slidesToShow: 2,
+                slidesToScroll: 1,
+            }
+        },
+        {
+            breakpoint: 992,
             settings: {
                 slidesToShow: 2,
                 slidesToScroll: 1,
+                autoplay: true,
             }
         },
         {
             breakpoint: 700,
             settings: {
                 slidesToShow: 1,
-                slidesToScroll: 1
+                slidesToScroll: 1,
+                autoplay: true,
             }
         },
 
@@ -35,3 +45,19 @@ $('.sliderbox').slick({
         // instead of a settings object
     ]
 });
+
+let scroll_button = document.querySelector('.scroll-button')
+
+function backtoTop() {
+    window.scrollTo({ top: 0, behavior: "smooth" })
+}
+
+window.addEventListener("scroll", function () {
+    if (window.scrollY > 200) {
+        scroll_button.setAttribute("style", "display: flex;")
+    } else {
+        scroll_button.setAttribute("style", "display: none;")
+
+    }
+
+})
